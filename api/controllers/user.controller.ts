20dff0493efa0e5519df171b70dc2d2db1aba108
Gonzalo Nicolás Argüello <arguellogonzalo97@gmail.com>
2023-02-10 17:20:16 -0300

    Vista de user mobile

--- api/controllers/user.controller.ts
+++ api/controllers/user.controller.ts
@@ -36,11 +36,11 @@ const addUser = async (req: Request, res: Response) => {
   const userUsername = await User.find({ username });
   try {
     if (userEmail.length > 0 && userUsername.length > 0)
-      return res.status(403).json({ message: "El usuario y email ya existe" });
+      return res.status(403).json({ message: "The username and email already exists" });
     if (userUsername.length > 0)
-      return res.status(403).json({ message: "El usuario ya existe" });
+      return res.status(403).json({ message: "User already exists" });
     if (userEmail.length > 0)
-      return res.status(403).json({ message: "El email ya existe" });
+      return res.status(403).json({ message: "The email already exists" });
 
     const newUser = new User({
       username,
@@ -58,7 +58,7 @@ const addUser = async (req: Request, res: Response) => {
       await transporter.sendMail({
         from: `The Perfect Mentor <perfect.mentor.p5@gmail.com>`,
         to: email,
-        subject: "Verificar mail",
+        subject: "Verify email",
         text: "...",
         html: template,
       });
@@ -66,7 +66,7 @@ const addUser = async (req: Request, res: Response) => {
       res.status(500).json({ message: error });
     }
     await newUser.save();
-    res.status(201).json({ message: "Usuario creado" });
+    res.status(201).json({ message: "User created" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -83,8 +83,8 @@ const updateUser = async (req: Request, res: Response) => {
       );
     }
     const updatedUser = await User.findByIdAndUpdate(id, body, { new: true });
-    if (!updatedUser) throw new CustomError("El usuario no existe", 404);
-    res.status(200).json({ message: "Usuario actualizado", updatedUser });
+    if (!updatedUser) throw new CustomError("User not exist", 404);
+    res.status(200).json({ message: "Updated user", updatedUser });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -95,11 +95,11 @@ const deleteUser = async (req: Request, res: Response) => {
   const user = await User.findById(idUser);
   const role = await Role.findById(user.role);
   try {
-    if (!role) throw new CustomError("No existe rol especificado", 404);
+    if (!role) throw new CustomError("No role specified", 404);
     if (role.role !== "ADMIN")
-      throw new CustomError("No tiene los permisos necesarios", 404);
+      throw new CustomError("You do not have the necessary permissions", 404);
     await User.findByIdAndDelete(idUserToDelete);
-    res.status(200).json({ message: "El usuario ha sido eliminado" });
+    res.status(200).json({ message: "The user has been removed" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -109,19 +109,17 @@ const loginUser = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    if (!user) throw new CustomError("Usuario no encontrado", 404);
-    if (!user.verify) throw new CustomError("Debes verificar tu cuenta", 401);
+    if (!user) throw new CustomError("User not found", 404);
+    if (!user.verify) throw new CustomError("You must verify your account", 401);
     const passwordOk = user.validatePassword(password);
-    if (!passwordOk) throw new CustomError("Credenciales inválidas", 401);
+    if (!passwordOk) throw new CustomError("Invalid credentials", 401);
     const payload = {
       email: user.email,
-      name: user.name,
-      lastname: user.lastname,
       id: user.id,
     };
     const token = generateToken(payload);
     res.cookie("token", token);
-    res.status(201).json({ message: "Logueo correcto", token });
+    res.status(201).json({ message: "Correct login", token, payload });
   } catch (error) {
     res.status(error.code || 500).json({ message: error.message });
   }
@@ -146,11 +144,11 @@ const verifyUser = async (req: Request, res: Response) => {
     const { email } = data.user;
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({ message: "Usuario no existe" });
+      return res.status(400).json({ message: "User not exist" });
     }
     user.verify = true;
     await user.save();
-    res.status(200).json({message:"Usuario verificado"})
+    res.status(200).json({message:"Verified user"})
   } catch (error) {
     res.status(error.code || 500).json({ message: error.message });
   }
--- api/server.ts
+++ api/server.ts
@@ -6,13 +6,19 @@ import router from "./routes/index.routes";
 import createRoles from "./config/initialSetup";
 import morgan from "morgan"
 
+const corsOptions = {
+  origin: ["http://localhost:3001"],
+  //update: or "origin: true," if you don't wanna add a specific one
+  credentials: true,
+};
+
 const app = express();
 dotenv.config();
 const PORT = process.env.SERVER_PORT || 3000;
 connectDB();
 createRoles();
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(morgan("dev"))
 app.use(express.urlencoded({ extended: true }));
 
