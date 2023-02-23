cf92eb412cc9f782ab22af14c087675303031948
Gonzalo Nicolás Argüello <arguellogonzalo97@gmail.com>
2023-02-23 18:25:33 -0300

    Avances

--- client/components/Navbar/Navbar.tsx
+++ client/components/Navbar/Navbar.tsx
@@ -5,6 +5,9 @@ import vector2 from "../../assets/img/Navbar/Vector2.svg";
 import frame5 from "../../assets/img/Navbar/Frame 5.svg";
 import frame6 from "../../assets/img/Navbar/Frame 6.svg";
 import frame7 from "../../assets/img/Navbar/Frame 7.svg";
+import stadistics from "../../assets/img/Navbar/Rectangle92.svg"
+import reports from "../../assets/img/Navbar/Vector8.svg"
+import profile from "../../assets/img/Navbar/Group163.svg"
 import Vector from "../../assets/img/The-Perfect-Mentor.svg";
 import Link from "next/link";
 import useMediaQuery from "../../hooks/useMediaQuey";
@@ -56,19 +59,19 @@ const Navbar = () => {
         </div>
         <div>
           <Link href="/stadistics">
-            <Image src={frame5} alt="frame5" className={styles.iconMenu} />
+            <Image src={stadistics} alt="frame5" className={styles.iconMenu} />
           </Link>
           <p>Stadistics</p>
         </div>
         <div>
           <Link href="/reports">
-            <Image src={frame6} alt="frame6" className={styles.iconMenu}  />
+            <Image src={reports} alt="frame6" className={styles.iconMenu}  />
           </Link>
           <p>Reports</p>
         </div>
         <div>
           <Link href="/profile">
-            <Image src={frame7} alt="frame7" className={styles.iconMenu}  />
+            <Image src={profile} alt="frame7" className={styles.iconMenu}  />
           </Link>
           <p>Profile</p>
         </div>
